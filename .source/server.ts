d80c077eeab40c58f478ad323477a9a4b3460f52
Marco Kuidja <kuidjamarco@gmail.com>
2026-02-04 20:50:48 +0100

    docs: add documentation content and metadata

---
+++ .source/server.ts
@@ -0,0 +1,30 @@
+// @ts-nocheck
+import { default as __fd_glob_17 } from "../content/docs/meta.json?collection=meta"
+import * as __fd_glob_16 from "../content/docs/sessions/stores.mdx?collection=docs"
+import * as __fd_glob_15 from "../content/docs/sessions/config.mdx?collection=docs"
+import * as __fd_glob_14 from "../content/docs/providers/google.mdx?collection=docs"
+import * as __fd_glob_13 from "../content/docs/providers/github.mdx?collection=docs"
+import * as __fd_glob_12 from "../content/docs/providers/discord.mdx?collection=docs"
+import * as __fd_glob_11 from "../content/docs/frameworks/axum.mdx?collection=docs"
+import * as __fd_glob_10 from "../content/docs/frameworks/actix.mdx?collection=docs"
+import * as __fd_glob_9 from "../content/docs/flows/oauth2.mdx?collection=docs"
+import * as __fd_glob_8 from "../content/docs/flows/device-flow.mdx?collection=docs"
+import * as __fd_glob_7 from "../content/docs/flows/credentials.mdx?collection=docs"
+import * as __fd_glob_6 from "../content/docs/flows/client-credentials.mdx?collection=docs"
+import * as __fd_glob_5 from "../content/docs/core/traits.mdx?collection=docs"
+import * as __fd_glob_4 from "../content/docs/core/identity.mdx?collection=docs"
+import * as __fd_glob_3 from "../content/docs/core/errors.mdx?collection=docs"
+import * as __fd_glob_2 from "../content/docs/quickstart.mdx?collection=docs"
+import * as __fd_glob_1 from "../content/docs/installation.mdx?collection=docs"
+import * as __fd_glob_0 from "../content/docs/index.mdx?collection=docs"
+import { server } from 'fumadocs-mdx/runtime/server';
+import type * as Config from '../source.config';
+
+const create = server<typeof Config, import("fumadocs-mdx/runtime/types").InternalTypeConfig & {
+  DocData: {
+  }
+}>({"doc":{"passthroughs":["extractedReferences"]}});
+
+export const docs = await create.doc("docs", "content/docs", {"index.mdx": __fd_glob_0, "installation.mdx": __fd_glob_1, "quickstart.mdx": __fd_glob_2, "core/errors.mdx": __fd_glob_3, "core/identity.mdx": __fd_glob_4, "core/traits.mdx": __fd_glob_5, "flows/client-credentials.mdx": __fd_glob_6, "flows/credentials.mdx": __fd_glob_7, "flows/device-flow.mdx": __fd_glob_8, "flows/oauth2.mdx": __fd_glob_9, "frameworks/actix.mdx": __fd_glob_10, "frameworks/axum.mdx": __fd_glob_11, "providers/discord.mdx": __fd_glob_12, "providers/github.mdx": __fd_glob_13, "providers/google.mdx": __fd_glob_14, "sessions/config.mdx": __fd_glob_15, "sessions/stores.mdx": __fd_glob_16, });
+
+export const meta = await create.meta("meta", "content/docs", {"meta.json": __fd_glob_17, });
