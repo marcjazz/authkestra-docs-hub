99365e28a8f8345d2c5ee40559e040f069c3abed
Marco Kuidja <kuidjamarco@gmail.com>
2026-02-07 17:36:29 +0100

    misc: update generated files and package configuration

--- .source/server.ts
+++ .source/server.ts
@@ -5,18 +5,18 @@ import * as __fd_glob_18 from "../content/docs/sessions/config.mdx?collection=do
 import * as __fd_glob_17 from "../content/docs/providers/google.mdx?collection=docs"
 import * as __fd_glob_16 from "../content/docs/providers/github.mdx?collection=docs"
 import * as __fd_glob_15 from "../content/docs/providers/discord.mdx?collection=docs"
-import * as __fd_glob_14 from "../content/docs/frameworks/axum.mdx?collection=docs"
-import * as __fd_glob_13 from "../content/docs/frameworks/actix.mdx?collection=docs"
-import * as __fd_glob_12 from "../content/docs/guides/oidc.mdx?collection=docs"
-import * as __fd_glob_11 from "../content/docs/guides/jwt-validation.mdx?collection=docs"
-import * as __fd_glob_10 from "../content/docs/core/traits.mdx?collection=docs"
-import * as __fd_glob_9 from "../content/docs/core/identity.mdx?collection=docs"
-import * as __fd_glob_8 from "../content/docs/core/errors.mdx?collection=docs"
-import * as __fd_glob_7 from "../content/docs/flows/pkce.mdx?collection=docs"
-import * as __fd_glob_6 from "../content/docs/flows/oauth2.mdx?collection=docs"
-import * as __fd_glob_5 from "../content/docs/flows/device-flow.mdx?collection=docs"
-import * as __fd_glob_4 from "../content/docs/flows/credentials.mdx?collection=docs"
-import * as __fd_glob_3 from "../content/docs/flows/client-credentials.mdx?collection=docs"
+import * as __fd_glob_14 from "../content/docs/guides/oidc.mdx?collection=docs"
+import * as __fd_glob_13 from "../content/docs/guides/jwt-validation.mdx?collection=docs"
+import * as __fd_glob_12 from "../content/docs/flows/pkce.mdx?collection=docs"
+import * as __fd_glob_11 from "../content/docs/flows/oauth2.mdx?collection=docs"
+import * as __fd_glob_10 from "../content/docs/flows/device-flow.mdx?collection=docs"
+import * as __fd_glob_9 from "../content/docs/flows/credentials.mdx?collection=docs"
+import * as __fd_glob_8 from "../content/docs/flows/client-credentials.mdx?collection=docs"
+import * as __fd_glob_7 from "../content/docs/frameworks/axum.mdx?collection=docs"
+import * as __fd_glob_6 from "../content/docs/frameworks/actix.mdx?collection=docs"
+import * as __fd_glob_5 from "../content/docs/core/traits.mdx?collection=docs"
+import * as __fd_glob_4 from "../content/docs/core/identity.mdx?collection=docs"
+import * as __fd_glob_3 from "../content/docs/core/errors.mdx?collection=docs"
 import * as __fd_glob_2 from "../content/docs/quickstart.mdx?collection=docs"
 import * as __fd_glob_1 from "../content/docs/installation.mdx?collection=docs"
 import * as __fd_glob_0 from "../content/docs/index.mdx?collection=docs"
@@ -28,6 +28,6 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.doc("docs", "content/docs", {"index.mdx": __fd_glob_0, "installation.mdx": __fd_glob_1, "quickstart.mdx": __fd_glob_2, "flows/client-credentials.mdx": __fd_glob_3, "flows/credentials.mdx": __fd_glob_4, "flows/device-flow.mdx": __fd_glob_5, "flows/oauth2.mdx": __fd_glob_6, "flows/pkce.mdx": __fd_glob_7, "core/errors.mdx": __fd_glob_8, "core/identity.mdx": __fd_glob_9, "core/traits.mdx": __fd_glob_10, "guides/jwt-validation.mdx": __fd_glob_11, "guides/oidc.mdx": __fd_glob_12, "frameworks/actix.mdx": __fd_glob_13, "frameworks/axum.mdx": __fd_glob_14, "providers/discord.mdx": __fd_glob_15, "providers/github.mdx": __fd_glob_16, "providers/google.mdx": __fd_glob_17, "sessions/config.mdx": __fd_glob_18, "sessions/stores.mdx": __fd_glob_19, });
+export const docs = await create.doc("docs", "content/docs", {"index.mdx": __fd_glob_0, "installation.mdx": __fd_glob_1, "quickstart.mdx": __fd_glob_2, "core/errors.mdx": __fd_glob_3, "core/identity.mdx": __fd_glob_4, "core/traits.mdx": __fd_glob_5, "frameworks/actix.mdx": __fd_glob_6, "frameworks/axum.mdx": __fd_glob_7, "flows/client-credentials.mdx": __fd_glob_8, "flows/credentials.mdx": __fd_glob_9, "flows/device-flow.mdx": __fd_glob_10, "flows/oauth2.mdx": __fd_glob_11, "flows/pkce.mdx": __fd_glob_12, "guides/jwt-validation.mdx": __fd_glob_13, "guides/oidc.mdx": __fd_glob_14, "providers/discord.mdx": __fd_glob_15, "providers/github.mdx": __fd_glob_16, "providers/google.mdx": __fd_glob_17, "sessions/config.mdx": __fd_glob_18, "sessions/stores.mdx": __fd_glob_19, });
 
 export const meta = await create.meta("meta", "content/docs", {"meta.json": __fd_glob_20, });
